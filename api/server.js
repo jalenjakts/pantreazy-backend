df00f2b0d8c2d49d12a3f1d5ea5f8a19268e045a
BenDawit <dawitpieliker@gmail.com>
2022-02-20 16:05:40 -0500

    boilerplate for sql api

---
+++ api/server.js
@@ -0,0 +1,27 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const errorHandler = require('api/_middleware/error-handler');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// allow cors requests from any origin and with credentials
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+
+// api routes
+app.use('/accounts', require('api/accounts/accounts.controller'));
+
+// swagger docs route
+app.use('/api-docs', require('api/_helpers/swagger'));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(port, () => console.log('Server listening on port ' + port));
