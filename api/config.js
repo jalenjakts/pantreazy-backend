6dd7419824bd332cdf88879a6520c9f0622871eb
BenDawit <dawitpieliker@gmail.com>
2022-04-17 13:19:54 -0400

    correcting sendEmail function

--- api/config.js
+++ api/config.js
@@ -14,6 +14,7 @@ const config = {
     "smtpOptions": {
         "host": "smtp.gmail.com",
         "port": 587,
+        "secure": false,
         "auth": {
             "user": process.env.EMAIL,
             "pass": process.env.EMAIL_PASS
